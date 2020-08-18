a069dcf39cf9df271ac2f74f324a2973ee166ea5
Michael Sprague <mikesprague@gmail.com>
2020-08-18 13:27:31 -0400

    :bug: Fix bug when no hourly data cached

--- src/components/Hourly.js
+++ src/components/Hourly.js
@@ -14,28 +14,29 @@ export const Hourly = ({ date }) => {
   const { lat, lng } = getData('coordinates');
 
   useEffect(() => {
-    setIsLoading(true);
+    if (lat && lng) {
+      setIsLoading(true);
 
-    const getWeatherData = async (lat, lng) => {
-      const weatherApiurl = `${apiUrl()}/location-and-weather/?lat=${lat}&lng=${lng}&time=${date}`;
-      console.log(weatherApiurl);
-      const weatherApiData =  await axios
-        .get(weatherApiurl)
-        .then(response => response.data);
-      setHourlyData({
-        lastUpdated: dayjs().toString(),
-        data: weatherApiData.weather.hourly.data,
-      });
-      setIsLoading(false);
-    };
+      const getWeatherData = async (lat, lng) => {
+        const weatherApiurl = `${apiUrl()}/location-and-weather/?lat=${lat}&lng=${lng}&time=${date}`;
+        const weatherApiData =  await axios
+          .get(weatherApiurl)
+          .then(response => response.data);
+        setHourlyData({
+          lastUpdated: dayjs().toString(),
+          data: weatherApiData.weather.hourly.data,
+        });
+        setIsLoading(false);
+      };
 
-    if (hourlyData && hourlyData.lastUpdated) {
-      const nextUpdateTime = dayjs(hourlyData.lastUpdated).add(20, 'minute');
-      if (dayjs().isAfter(nextUpdateTime)) {
+      if (hourlyData && hourlyData.lastUpdated) {
+        const nextUpdateTime = dayjs(hourlyData.lastUpdated).add(20, 'minute');
+        if (dayjs().isAfter(nextUpdateTime)) {
+          getWeatherData(lat, lng);
+        }
+      } else {
         getWeatherData(lat, lng);
       }
-    } else {
-      getWeatherData(lat, lng);
     }
 
     // return () => {};
@@ -45,11 +46,9 @@ export const Hourly = ({ date }) => {
   return (
     <div className="hourly-container">
       <ul className="hourly">
-      {console.log(typeof hourlyData, hourlyData)}
       {hourlyData && hourlyData.data.map((hour, index) => {
         const isFirst = index === 0;
         const isLast = index === 22;
-        console.log('index: ', index);
         const showSummary = hourlyData.data && formatSummary(hour, hourlyData.data, index, 0).length;
         return (index % 2 === 0) ? (
           <Hour key={nanoid(7)} data={hour} showSummary={showSummary} isFirst={isFirst} isLast={isLast} />
