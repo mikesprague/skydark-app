81ae3e8a5367f6472da602e947b5770968b75481
Michael Sprague <mikesprague@gmail.com>
2020-08-20 11:31:09 -0400

    :wrench: Opt-in to upcoming changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,9 @@ module.exports = {
   theme: {
     extend: {},
   },
+  future: {
+    removeDeprecatedGapUtilities: true,
+  },
   variants: {},
   plugins: [
     require('@tailwindcss/typography'),
