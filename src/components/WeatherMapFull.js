80f7b1f3639678eb4c0d99ef4def6915f13b9c95
Michael Sprague <mikesprague@gmail.com>
2020-08-25 14:11:29 -0400

    :recycle: Misc tweaks

--- src/components/WeatherMapFull.js
+++ src/components/WeatherMapFull.js
@@ -4,36 +4,15 @@ import { getData } from '../modules/local-storage';
 import './WeatherMapFull.scss';
 
 export const WeatherMapFull = (props) => {
-  const [mapLayerLabel, setMapLayerLabel] = useState('Precipitation');
   const coordinates = getData('coordinates') || null;
-
-  useEffect(() => {
-
-
-    return () => {};
-  }, [mapLayerLabel]);
-
-  const controlEl = Array.from(document.querySelectorAll('.leaflet-control-layers-selector'));
-  controlEl.forEach(el => {
-    el.addEventListener('change', (event) => {
-      if (event.target.checked) {
-        setMapLayerLabel(event.target.labels[0].innerText.trim())
-      }
-    });
-  });
 
   return (
     <div className="contents">
-      <div className="header">
-        <div className="section-name">
-          <h1>{mapLayerLabel}</h1>
-        </div>
-      </div>
       <div className="h-full min-h-screen contents v-full">
         <div className="min-h-screen map-container">
           <Map
             center={[coordinates.lat, coordinates.lng]}
-            zoom={10}
+            zoom={7}
             doubleClickZoom={true}
             dragging={true}
             keyboard={false}
@@ -45,37 +24,41 @@ export const WeatherMapFull = (props) => {
               opacity={0.6}
               zIndex={1}
             />
-            <WMSTileLayer
-              layers={['precipitation_new']}
-              url={`https://tile.openweathermap.org/map/{layers}/{z}/{x}/{y}.png?appid=${props.OPENWEATHERMAP_API_KEY}`}
-              opacity={1}
-              zIndex={10}
-            />
             <Marker position={[coordinates.lat, coordinates.lng]} opacity={.85} />
             <LayersControl position="topright">
               <LayersControl.BaseLayer name="Precipitation" checked>
                 <WMSTileLayer
                   url={`https://tile.openweathermap.org/map/precipitation_new/{z}/{x}/{y}.png?appid=${props.OPENWEATHERMAP_API_KEY}`}
+                  opacity={1}
+                  zIndex={10}
                 />
               </LayersControl.BaseLayer>
               <LayersControl.BaseLayer name="Clouds">
                 <WMSTileLayer
                   url={`https://tile.openweathermap.org/map/clouds_new/{z}/{x}/{y}.png?appid=${props.OPENWEATHERMAP_API_KEY}`}
+                  opacity={1}
+                  zIndex={10}
                 />
               </LayersControl.BaseLayer>
               <LayersControl.BaseLayer name="Temperature">
                 <WMSTileLayer
                   url={`https://tile.openweathermap.org/map/temp_new/{z}/{x}/{y}.png?appid=${props.OPENWEATHERMAP_API_KEY}`}
+                  opacity={1}
+                  zIndex={10}
                 />
               </LayersControl.BaseLayer>
               <LayersControl.BaseLayer name="Wind Speed">
                 <WMSTileLayer
                   url={`https://tile.openweathermap.org/map/wind_new/{z}/{x}/{y}.png?appid=${props.OPENWEATHERMAP_API_KEY}`}
+                  opacity={1}
+                  zIndex={10}
                 />
               </LayersControl.BaseLayer>
               <LayersControl.BaseLayer name="Sea Level Pressure">
                 <WMSTileLayer
                   url={`https://tile.openweathermap.org/map/pressure_new/{z}/{x}/{y}.png?appid=${props.OPENWEATHERMAP_API_KEY}`}
+                  opacity={1}
+                  zIndex={10}
                 />
               </LayersControl.BaseLayer>
             </LayersControl>
