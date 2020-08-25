f121c7eabaf9841a2d2af79f56f898c1c4ffb974
Michael Sprague <mikesprague@gmail.com>
2020-08-25 17:28:05 -0400

    :recycle: Add header with dropdown for map layers
    Removes in-map version previously active

--- src/components/WeatherMapFull.js
+++ src/components/WeatherMapFull.js
@@ -4,10 +4,32 @@ import { getData } from '../modules/local-storage';
 import './WeatherMapFull.scss';
 
 export const WeatherMapFull = (props) => {
+  const [mapView, setMapView] = useState('precipitation_new');
   const coordinates = getData('coordinates') || null;
+
+  useEffect(() => {
+
+    return () => {};
+  }, [mapView]);
+
+  const changeHandler = (event) => {
+    console.log(event.target.value);
+    setMapView(event.target.value);
+  };
 
   return (
     <div className="contents">
+      <div className="header">
+        <div className="section-name">
+          <select className="" onChange={changeHandler}>
+            <option value="precipitation_new">Precipitation</option>
+            <option value="clouds_new">Clouds</option>
+            <option value="temp_new">Temperature</option>
+            <option value="wind_new">Wind Speed</option>
+            <option value="pressure_new">Sea Level Pressure</option>
+          </select>
+        </div>
+      </div>
       <div className="h-full min-h-screen contents v-full">
         <div className="min-h-screen map-container">
           <Map
@@ -24,8 +46,11 @@ export const WeatherMapFull = (props) => {
               opacity={0.6}
               zIndex={1}
             />
+            <WMSTileLayer
+              url={`https://tile.openweathermap.org/map/${mapView}/{z}/{x}/{y}.png?appid=${props.OPENWEATHERMAP_API_KEY}`}
+            />
             <Marker position={[coordinates.lat, coordinates.lng]} opacity={.85} />
-            <LayersControl position="topright">
+            {/* <LayersControl position="topright">
               <LayersControl.BaseLayer name="Precipitation" checked>
                 <WMSTileLayer
                   url={`https://tile.openweathermap.org/map/precipitation_new/{z}/{x}/{y}.png?appid=${props.OPENWEATHERMAP_API_KEY}`}
@@ -61,7 +86,7 @@ export const WeatherMapFull = (props) => {
                   zIndex={10}
                 />
               </LayersControl.BaseLayer>
-            </LayersControl>
+            </LayersControl> */}
           </Map>
         </div>
       </div>
