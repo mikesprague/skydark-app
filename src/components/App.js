8670ad970ad4abca280b96199e4d81be15fd6f02
Michael Sprague <mikesprague@gmail.com>
2020-09-03 10:59:06 -0400

    :rotating_light: Fix linter warning

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,9 @@
 import React from "react";
 import { hot } from 'react-hot-loader/root';
-import { Map, Marker, TileLayer, WMSTileLayer } from "react-leaflet";
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import {
   HashRouter as Router,
   Switch,
   Route,
-  NavLink,
 } from "react-router-dom";
 import { Footer } from '../components/Footer';
 import { Forecast } from '../components/Forecast';
