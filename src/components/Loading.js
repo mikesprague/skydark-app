4b9367c73c4d25a7166af2c342b8a04a0121768f
Michael Sprague <mikesprague@gmail.com>
2020-09-29 11:07:34 -0400

    :children_crossing: Improve loading component

--- src/components/Loading.js
+++ src/components/Loading.js
@@ -1,13 +1,29 @@
+import PropTypes from 'prop-types';
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
-export const Loading = () => {
+export const Loading = ({ fullHeight }) => {
+  let classList = 'flex items-center justify-center h-100 w-100 loading-container';
+
+  if (fullHeight) {
+    classList = `${classList} min-h-screen`;
+  }
+
+  console.log(classList);
 
   return (
-    <div className="flex items-center justify-center min-h-screen h-100 w-100 loading-container">
-      <FontAwesomeIcon icon={['fad', 'spinner']} size="3x" pulse />
+    <div className={classList}>
+      <FontAwesomeIcon icon={['fad', 'spinner']} size="3x" className="m-8" pulse />
     </div>
   );
+};
+
+Loading.defaultProps = {
+  fullHeight: true,
+};
+
+Loading.propTypes = {
+  fullHeight: PropTypes.bool,
 };
 
 export default Loading;
