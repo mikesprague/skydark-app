06c71977f96294bd2be2e56dea83b9b83ccf3b29
Michael Sprague <mikesprague@gmail.com>
2020-10-19 21:29:55 -0400

    :recycle: Comment out empty return function

--- src/components/Hour.js
+++ src/components/Hour.js
@@ -13,7 +13,7 @@ export const Hour = (props) => {
   useEffect(() => {
     setHourlyConditionToShow(conditionToShow);
 
-    return () => {};
+    // return () => {};
   }, [hourlyConditionToShow, conditionToShow]);
 
   return (
