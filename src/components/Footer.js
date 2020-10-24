52a96492f22cec24b09486f4980cb9d611db1025
Michael Sprague <mikesprague@gmail.com>
2020-10-24 10:35:44 -0400

    :recycle: Remove attribute

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -7,21 +7,21 @@ export const Footer = () => (
   <div className="footer">
     <div className="flex text-sm">
       <div className="footer-column">
-        <NavLink activeClassName="active" exact="true" to="/">
+        <NavLink activeClassName="active" to="/" end>
           <FontAwesomeIcon icon={['fad', 'house-day']} className="footer-icon" fixedWidth />
           <br />
           <small>Forecast</small>
         </NavLink>
       </div>
       <div className="footer-column">
-        <NavLink activeClassName="active" exact="true" to="/map">
+        <NavLink activeClassName="active" to="/map">
           <FontAwesomeIcon icon={['fad', 'globe-stand']} className="footer-icon" fixedWidth />
           <br />
           <small>Map</small>
         </NavLink>
       </div>
       <div className="footer-column">
-        <NavLink activeClassName="active" exact="true" to="/about">
+        <NavLink activeClassName="active" to="/about">
           <FontAwesomeIcon icon={['fad', 'info-circle']} className="footer-icon" fixedWidth />
           <br />
           <small>About</small>
