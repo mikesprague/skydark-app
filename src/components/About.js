89669dee09ec2531ebdc3ff3fb599dd834a34715
Michael Sprague <mikesprague@gmail.com>
2020-10-27 13:21:48 -0400

    :recycle: Start building out About content

--- src/components/About.js
+++ src/components/About.js
@@ -8,8 +8,27 @@ export const About = () => (
         <h1>About</h1>
       </div>
     </div>
-    <div className="my-16">
-      <p>Work in progress. This page will have content soon ...</p>
+    <div className="about-content">
+      <div>
+        <h2>Background</h2>
+        <p>
+          {'Sky Dark is an open source '}
+          <abbr title="Progressive Web Application">PWA</abbr>
+          {' built as an homage to the now defunct Dark Sky for Android app.'}
+        </p>
+        <p>
+          Please note that this is a work in progress and is NOT an exact replica in functionality or design.
+          I built it mainly to learn a few new things and because the Dark Sky for Android app used to be my
+          daily driver and I missed the experience.
+        </p>
+        <h2>Issues/Bugs</h2>
+        <p>Errors are tracked automatically with Bugsnag.</p>
+        <p>
+          Bug reports can be filed manually here:
+          <br />
+          <a href="https://github.com/mikesprague/skydark-app/issues" rel="noopener noreferrer" target="_blank">https://github.com/mikesprague/skydark-app/issues</a>
+        </p>
+      </div>
     </div>
   </div>
 );
