0e4a0c30f63b0f0c7cb6456eb61b71aafff7149f
Michael Sprague <mikesprague@gmail.com>
2020-11-11 11:30:13 -0500

    :zap: Add suspense/lazy to App component

--- src/components/App.js
+++ src/components/App.js
@@ -1,39 +1,48 @@
 import PropTypes from 'prop-types';
-import React from 'react';
+import React, { Suspense, lazy } from 'react';
 import { hot } from 'react-hot-loader/root';
 import {
   HashRouter as Router,
   Routes,
   Route,
 } from 'react-router-dom';
-import { About } from './About';
-import { Footer } from './Footer';
-import { Forecast } from './Forecast';
-import { Settings } from './Settings';
-import { WeatherMapFull } from './WeatherMapFull';
+// import { About } from './About';
+// import { Footer } from './Footer';
+// import { Forecast } from './Forecast';
+// import { Settings } from './Settings';
+// import { WeatherMapFull } from './WeatherMapFull';
+import { Loading } from './Loading';
 import { initIcons } from '../modules/helpers';
 import './App.scss';
+
+const Forecast = lazy(() => import('./Forecast'));
+const Settings = lazy(() => import('./Settings'));
+const About = lazy(() => import('./About'));
+const WeatherMapFull = lazy(() => import('./WeatherMapFull'));
+const Footer = lazy(() => import('./Footer'));
 
 export const App = ({ OPENWEATHERMAP_API_KEY }) => {
   initIcons();
 
   return (
     <Router>
-      <Routes>
-        <Route path="/about">
-          <About />
-        </Route>
-        <Route path="/map">
-          <WeatherMapFull OPENWEATHERMAP_API_KEY={OPENWEATHERMAP_API_KEY} />
-        </Route>
-        <Route path="/settings">
-          <Settings />
-        </Route>
-        <Route path="/" end>
-          <Forecast />
-        </Route>
-      </Routes>
-      <Footer />
+      <Suspense fallback={<Loading fullHeight={true} />}>
+        <Routes>
+          <Route path="/about">
+            <About />
+          </Route>
+          <Route path="/map">
+            <WeatherMapFull OPENWEATHERMAP_API_KEY={OPENWEATHERMAP_API_KEY} />
+          </Route>
+          <Route path="/settings">
+            <Settings />
+          </Route>
+          <Route path="/" end>
+            <Forecast />
+          </Route>
+        </Routes>
+        <Footer />
+      </Suspense>
     </Router>
   );
 };
