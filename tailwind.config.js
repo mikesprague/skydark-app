16cf8c7ad8a78d926fda1155804dca2aa91ae09e
Michael Sprague <mikesprague@gmail.com>
2020-11-19 09:54:10 -0500

    :recycle: Add tailwind colors to config (fixes missing orange in v2)

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,42 @@
+const colors = require('tailwindcss/colors');
+
 module.exports = {
   purge: false,
   theme: {
-    extend: {},
+    colors: {
+      white: '#fff',
+      black: '#000',
+      bluegray: colors.blueGray,
+      coolgray: colors.coolGray,
+      gray: colors.gray,
+      truegray: colors.trueGray,
+      warmgray: colors.warmGray,
+      red: colors.red,
+      orange: colors.orange,
+      amber: colors.amber,
+      yellow: colors.yellow,
+      lime: colors.lime,
+      green: colors.green,
+      emerald: colors.emerald,
+      teal: colors.teal,
+      cyan: colors.cyan,
+      lightblue: colors.lightBlue,
+      blue: colors.blue,
+      indigo: colors.indigo,
+      violet: colors.violet,
+      purple: colors.purple,
+      pink: colors.pink,
+      rose: colors.rose,
+      transparent: colors.transparent,
+    },
+    extend: {
+      inset: {
+        15: '3.75rem',
+      },
+      spacing: {
+        15: '3.75rem',
+      },
+    },
   },
   variants: {
     extend: {},
