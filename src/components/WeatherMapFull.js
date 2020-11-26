16f95c75745e970a29e457815138744f752f2ead
Michael Sprague <mikesprague@gmail.com>
2020-11-26 12:02:39 -0500

    :recycle: Improve slider code and move to bottom above footer

--- src/components/WeatherMapFull.js
+++ src/components/WeatherMapFull.js
@@ -95,7 +95,106 @@ export const WeatherMapFull = ({ OPENWEATHERMAP_API_KEY }) => {
   };
 
   return (
-    <div className="map-container">
+    <>
+      <div className="map-container">
+        <MapContainer
+          animate={true}
+          boxZoom={true}
+          center={[coordinates.lat, coordinates.lng]}
+          doubleClickZoom={true}
+          dragging={true}
+          className="weather-map-full"
+          keyboard={false}
+          whenCreated={createdHandler}
+          scrollWheelZoom={false}
+          tap={true}
+          touchZoom={true}
+          zoom={9}
+          zoomControl={false}
+        >
+          <Marker position={[coordinates.lat, coordinates.lng]} opacity={0.9} />
+          <ScaleControl position="topleft" />
+          <ZoomControl position="topleft" />
+          <AttributionControl position="topright" />
+          <LayersControl>
+            <LayersControl.BaseLayer name="Dark" checked>
+              <TileLayer
+                url="https://cartodb-basemaps-{s}.global.ssl.fastly.net/dark_all/{z}/{x}/{y}.png"
+                opacity={1}
+                attribution={'&copy; <a href="https://carto.com/" rel="noopener noreferrer" target="_blank">CARTO</a>'}
+              />
+            </LayersControl.BaseLayer>
+            <LayersControl.BaseLayer name="Color">
+              <TileLayer
+                url="https://cartodb-basemaps-{s}.global.ssl.fastly.net/rastertiles/voyager/{z}/{x}/{y}.png"
+                opacity={1}
+                attribution={'&copy; <a href="https://carto.com/" rel="noopener noreferrer" target="_blank">CARTO</a>'}
+              />
+            </LayersControl.BaseLayer>
+            <LayersControl.BaseLayer name="Light">
+              <TileLayer
+                url="https://cartodb-basemaps-{s}.global.ssl.fastly.net/light_all/{z}/{x}/{y}.png"
+                opacity={1}
+                attribution={'&copy; <a href="https://carto.com/" rel="noopener noreferrer" target="_blank">CARTO</a>'}
+              />
+            </LayersControl.BaseLayer>
+            <LayersControl.BaseLayer name="Street">
+              <TileLayer
+                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                opacity={1}
+                attribution={'&copy; <a href="https://osm.org/copyright" rel="noopener noreferrer" target="_blank">OpenStreetMap</a>'}
+              />
+            </LayersControl.BaseLayer>
+            <LayersControl.BaseLayer name="Street (Gray)">
+              <TileLayer
+                url="https://tiles.wmflabs.org/bw-mapnik/{z}/{x}/{y}.png"
+                opacity={1}
+                attribution={'&copy; <a href="https://osm.org/copyright" rel="noopener noreferrer" target="_blank">OpenStreetMap</a>'}
+              />
+            </LayersControl.BaseLayer>
+            <LayersControl.BaseLayer name="Black/White">
+              <TileLayer
+                url="https://stamen-tiles.a.ssl.fastly.net/toner/{z}/{x}/{y}@2x.png"
+                opacity={1}
+                attribution={'&copy; <a href="https://stamen.com" rel="noopener noreferrer" target="_blank">Stamen Design</a>'}
+              />
+            </LayersControl.BaseLayer>
+            <LayersControl.BaseLayer name="Black/White/Gray">
+              <TileLayer
+                url="https://stamen-tiles.a.ssl.fastly.net/toner-lite/{z}/{x}/{y}@2x.png"
+                opacity={1}
+                attribution={'&copy; <a href="https://stamen.com" rel="noopener noreferrer" target="_blank">Stamen Design</a>'}
+              />
+            </LayersControl.BaseLayer>
+            <LayersControl.BaseLayer name="Watercolor">
+              <TileLayer
+                url="https://stamen-tiles.a.ssl.fastly.net/watercolor/{z}/{x}/{y}.jpg"
+                opacity={1}
+                attribution={'&copy; <a href="https://stamen.com" rel="noopener noreferrer" target="_blank">Stamen Design</a>'}
+              />
+            </LayersControl.BaseLayer>
+            <LayersControl.Overlay name="Radar" checked>
+              <WMSTileLayer
+                url={`https://tilecache.rainviewer.com/v2/radar/${getRadarTs()}/512/{z}/{x}/{y}/8/1_1.png`}
+                attribution={'&copy; <a href="https://www.rainviewer.com/api.html" rel="noopener noreferrer" target="_blank">RainViewer</a>'}
+                ref={radarTileLayerRef}
+              />
+            </LayersControl.Overlay>
+            <LayersControl.Overlay name="Temperature">
+              <WMSTileLayer
+                url={`https://tile.openweathermap.org/map/temp_new/{z}/{x}/{y}.png?appid=${OPENWEATHERMAP_API_KEY}`}
+                attribution={'&copy; <a href="https://openweathermap.org/" rel="noopener noreferrer" target="_blank">OpenWeatherMap</a>'}
+              />
+            </LayersControl.Overlay>
+          </LayersControl>
+        </MapContainer>
+        {/* <div className="radar-key">
+          <img src="/images/radar-key.png" alt="Radar Key" />
+        </div>
+        <div className={mapView === 'temp_new' ? 'temp-key' : 'hidden'}>
+          <img src="/images/temp-key.png" alt="Temperature Key" />
+        </div> */}
+      </div>
       <div className="slider-container">
         <div className="slider">
           {tsData ? `${dayjs.unix(ts).format('h:mma')} ` : ''}
@@ -112,104 +211,7 @@ export const WeatherMapFull = ({ OPENWEATHERMAP_API_KEY }) => {
           ) : ''}
         </div>
       </div>
-      <MapContainer
-        animate={true}
-        boxZoom={true}
-        center={[coordinates.lat, coordinates.lng]}
-        doubleClickZoom={true}
-        dragging={true}
-        className="weather-map-full"
-        keyboard={false}
-        whenCreated={createdHandler}
-        scrollWheelZoom={false}
-        tap={true}
-        touchZoom={true}
-        zoom={9}
-        zoomControl={false}
-      >
-        <Marker position={[coordinates.lat, coordinates.lng]} opacity={0.9} />
-        <ScaleControl position="topleft" />
-        <ZoomControl position="topleft" />
-        <AttributionControl position="bottomleft" />
-        <LayersControl>
-          <LayersControl.BaseLayer name="Dark" checked>
-            <TileLayer
-              url="https://cartodb-basemaps-{s}.global.ssl.fastly.net/dark_all/{z}/{x}/{y}.png"
-              opacity={1}
-              attribution={'&copy; <a href="https://carto.com/" rel="noopener noreferrer" target="_blank">CARTO</a>'}
-            />
-          </LayersControl.BaseLayer>
-          <LayersControl.BaseLayer name="Color">
-            <TileLayer
-              url="https://cartodb-basemaps-{s}.global.ssl.fastly.net/rastertiles/voyager/{z}/{x}/{y}.png"
-              opacity={1}
-              attribution={'&copy; <a href="https://carto.com/" rel="noopener noreferrer" target="_blank">CARTO</a>'}
-            />
-          </LayersControl.BaseLayer>
-          <LayersControl.BaseLayer name="Light">
-            <TileLayer
-              url="https://cartodb-basemaps-{s}.global.ssl.fastly.net/light_all/{z}/{x}/{y}.png"
-              opacity={1}
-              attribution={'&copy; <a href="https://carto.com/" rel="noopener noreferrer" target="_blank">CARTO</a>'}
-            />
-          </LayersControl.BaseLayer>
-          <LayersControl.BaseLayer name="Street">
-            <TileLayer
-              url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-              opacity={1}
-              attribution={'&copy; <a href="https://osm.org/copyright" rel="noopener noreferrer" target="_blank">OpenStreetMap</a>'}
-            />
-          </LayersControl.BaseLayer>
-          <LayersControl.BaseLayer name="Street (Gray)">
-            <TileLayer
-              url="https://tiles.wmflabs.org/bw-mapnik/{z}/{x}/{y}.png"
-              opacity={1}
-              attribution={'&copy; <a href="https://osm.org/copyright" rel="noopener noreferrer" target="_blank">OpenStreetMap</a>'}
-            />
-          </LayersControl.BaseLayer>
-          <LayersControl.BaseLayer name="Black/White">
-            <TileLayer
-              url="https://stamen-tiles.a.ssl.fastly.net/toner/{z}/{x}/{y}@2x.png"
-              opacity={1}
-              attribution={'&copy; <a href="https://stamen.com" rel="noopener noreferrer" target="_blank">Stamen Design</a>'}
-            />
-          </LayersControl.BaseLayer>
-          <LayersControl.BaseLayer name="Black/White/Gray">
-            <TileLayer
-              url="https://stamen-tiles.a.ssl.fastly.net/toner-lite/{z}/{x}/{y}@2x.png"
-              opacity={1}
-              attribution={'&copy; <a href="https://stamen.com" rel="noopener noreferrer" target="_blank">Stamen Design</a>'}
-            />
-          </LayersControl.BaseLayer>
-          <LayersControl.BaseLayer name="Watercolor">
-            <TileLayer
-              url="https://stamen-tiles.a.ssl.fastly.net/watercolor/{z}/{x}/{y}.jpg"
-              opacity={1}
-              attribution={'&copy; <a href="https://stamen.com" rel="noopener noreferrer" target="_blank">Stamen Design</a>'}
-            />
-          </LayersControl.BaseLayer>
-          <LayersControl.Overlay name="Radar" checked>
-            <WMSTileLayer
-              url={`https://tilecache.rainviewer.com/v2/radar/${getRadarTs()}/512/{z}/{x}/{y}/8/1_1.png`}
-              attribution={'&copy; <a href="https://www.rainviewer.com/api.html" rel="noopener noreferrer" target="_blank">RainViewer</a>'}
-              ref={radarTileLayerRef}
-            />
-          </LayersControl.Overlay>
-          <LayersControl.Overlay name="Temperature">
-            <WMSTileLayer
-              url={`https://tile.openweathermap.org/map/temp_new/{z}/{x}/{y}.png?appid=${OPENWEATHERMAP_API_KEY}`}
-              attribution={'&copy; <a href="https://openweathermap.org/" rel="noopener noreferrer" target="_blank">OpenWeatherMap</a>'}
-            />
-          </LayersControl.Overlay>
-        </LayersControl>
-      </MapContainer>
-      {/* <div className="radar-key">
-        <img src="/images/radar-key.png" alt="Radar Key" />
-      </div>
-      <div className={mapView === 'temp_new' ? 'temp-key' : 'hidden'}>
-        <img src="/images/temp-key.png" alt="Temperature Key" />
-      </div> */}
-    </div>
+    </>
   );
 };
 
