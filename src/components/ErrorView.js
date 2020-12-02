65a61b68b5a29d180a854b0599b33bb13172f44f
Michael Sprague <mikesprague@gmail.com>
2020-12-02 10:27:23 -0500

    :recycle: Move ErrorView into own file

---
+++ src/components/ErrorView.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { resetData } from '../modules/local-storage';
+
+export const ErrorView = () => {
+  const clickHandler = () => {
+    resetData();
+    window.location.href = '/';
+  };
+
+  return (
+    <div className="w-full text-center">
+      <h3 className="text-2xl text-red-500">Sorry, an error has occured.</h3>
+      <br />
+      <br />
+      <button onClick={clickHandler} className="p-6 text-lg font-bold leading-loose text-red-500 bg-gray-200" type="button">&nbsp;&nbsp;Click Here to Reset and Reload&nbsp;&nbsp;</button>
+    </div>
+  );
+};
+
+export default ErrorView;
--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.scss';
 import { App } from './components/App';
-import { resetData } from './modules/local-storage';
+import { ErrorView } from './components/ErrorView';
 import {
   initServiceWorker,
   isDev,
@@ -16,23 +16,6 @@ Bugsnag.start({
 });
 
 const ErrorBoundary = Bugsnag.getPlugin('react').createErrorBoundary(React);
-
-const ErrorView = () => {
-  const clickHandler = (event) => {
-    // console.log(event);
-    resetData();
-    window.location.href = '/';
-  };
-
-  return (
-    <div className="w-full text-center">
-      <h3 className="text-2xl text-red-500">Sorry, an error has occured.</h3>
-      <br />
-      <br />
-      <button onClick={clickHandler} className="p-6 text-lg font-bold leading-loose text-red-500 bg-gray-200" type="button">&nbsp;&nbsp;Click Here to Reset and Reload&nbsp;&nbsp;</button>
-    </div>
-  );
-};
 
 const handleError = (error) => {
   if (isDev()) {
