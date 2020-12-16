48692b61b54dac4933e15d19f7b20e31e0f96a2a
Michael Sprague <mikesprague@gmail.com>
2020-12-16 17:48:24 -0500

    :recycle: Refactor precipitation chart into own component

--- src/components/NextHour.js
+++ src/components/NextHour.js
@@ -1,7 +1,7 @@
-import { Chart } from 'react-google-charts';
 import PropTypes from 'prop-types';
 import React, { useEffect, useState } from 'react';
 import './NextHour.scss';
+import { PrecipChart } from './PrecipChart';
 
 export const NextHour = ({ data }) => {
   const [summaryText, setSummaryText] = useState(null);
@@ -34,62 +34,9 @@ export const NextHour = ({ data }) => {
     return () => setNextHourPrecipitation(false);
   }, [summaryText]);
 
-  const [chartData, setChartData] = useState(null);
-  useEffect(() => {
-    if (!nextHourPrecipitation) { return; }
-
-    const dataArray = [
-      ['Minute', 'Precipitation'],
-    ];
-    data.minutely.data.forEach((minute, index) => {
-      dataArray.push([index, minute.precipIntensity]);
-    });
-    setChartData(dataArray);
-
-    return () => setChartData(null);
-  }, [nextHourPrecipitation, data]);
-
   return nextHourPrecipitation ? (
     <>
-      <Chart
-        width="100%"
-        height="140px"
-        chartType="AreaChart"
-        loader={<div className="text-center text-transparent">Loading...</div>}
-        data={chartData}
-        options={{
-          backgroundColor: 'transparent',
-          series: [
-            { color: '#76a9fa', areaOpacity: 0.75 },
-          ],
-          hAxis: {
-            baselineColor: 'transparent',
-            gridlines: { color: 'transparent', count: 5 },
-            textPosition: 'out',
-            textStyle: { color: '#999' },
-            ticks: [{ v: 0, f: '' }, { v: 10, f: '10 min' }, { v: 20, f: '20 min' }, { v: 30, f: '30 min' }, { v: 40, f: '40 min' }, { v: 50, f: '50 min' }, { v: 60, f: '' }],
-          },
-          vAxis: {
-            baselineColor: 'transparent',
-            gridlines: { color: '#999' },
-            textPosition: 'in',
-            textStyle: { color: '#ccc' },
-            ticks: [{ v: 0, f: '' }, { v: 0.1, f: 'LIGHT' }, { v: 0.2, f: 'MED' }, { v: 0.3, f: 'HEAVY' }],
-            viewWindow: { min: 0, max: 0.34 },
-            viewWindowMode: 'maximized',
-          },
-          tooltip: {
-            trigger: 'none',
-          },
-          enableInteractivity: false,
-          lineWidth: 0.25,
-          pointsVisible: false,
-          chartArea: { width: '100%', height: '140px', top: 0 },
-          titlePosition: 'in',
-          axisTitlesPosition: 'in',
-          legend: 'none',
-        }}
-      />
+      <PrecipChart data={data.minutely.data} />
       <p className="px-4 mb-4 -mt-6 text-base text-center">
         {`Next Hour: ${summaryText}`}
       </p>
---
+++ src/components/PrecipChart.js
@@ -0,0 +1,71 @@
+import { Chart } from 'react-google-charts';
+import PropTypes from 'prop-types';
+import React, { useEffect, useState } from 'react';
+import './PrecipChart.scss';
+
+export const PrecipChart = ({ data }) => {
+  const [chartData, setChartData] = useState(null);
+  useEffect(() => {
+    if (!data) { return; }
+
+    const dataArray = [
+      ['Minute', 'Precipitation'],
+    ];
+    data.forEach((minute, index) => {
+      dataArray.push([index, minute.precipIntensity]);
+    });
+    setChartData(dataArray);
+
+    return () => setChartData(null);
+  }, [data]);
+
+  return (
+    <Chart
+      width="100%"
+      height="140px"
+      chartType="AreaChart"
+      loader={<div className="text-center text-transparent">Loading...</div>}
+      data={chartData}
+      options={{
+        backgroundColor: 'transparent',
+        series: [
+          { color: '#76a9fa', areaOpacity: 0.75 },
+        ],
+        hAxis: {
+          baselineColor: 'transparent',
+          gridlines: { color: 'transparent', count: 5 },
+          textPosition: 'out',
+          textStyle: { color: '#999' },
+          ticks: [{ v: 0, f: '' }, { v: 10, f: '10 min' }, { v: 20, f: '20 min' }, { v: 30, f: '30 min' }, { v: 40, f: '40 min' }, { v: 50, f: '50 min' }, { v: 60, f: '' }],
+        },
+        vAxis: {
+          baselineColor: 'transparent',
+          gridlines: { color: '#999' },
+          textPosition: 'in',
+          textStyle: { color: '#ccc' },
+          ticks: [{ v: 0, f: '' }, { v: 0.1, f: 'LIGHT' }, { v: 0.2, f: 'MED' }, { v: 0.3, f: 'HEAVY' }],
+          viewWindow: { min: 0, max: 0.34 },
+          viewWindowMode: 'maximized',
+        },
+        tooltip: {
+          trigger: 'none',
+        },
+        enableInteractivity: false,
+        lineWidth: 0.25,
+        pointsVisible: false,
+        chartArea: { width: '100%', height: '140px', top: 0 },
+        titlePosition: 'in',
+        axisTitlesPosition: 'in',
+        legend: 'none',
+      }}
+    />
+  );
+};
+
+PrecipChart.propTypes = {
+  data: PropTypes.arrayOf(
+    PropTypes.oneOfType([PropTypes.string, PropTypes.number, PropTypes.array, PropTypes.object]),
+  ).isRequired,
+};
+
+export default PrecipChart;
