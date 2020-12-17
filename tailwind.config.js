800094600897961f4086deae13fb88e4e2d5f3c7
Michael Sprague <mikesprague@gmail.com>
2020-12-17 18:56:41 -0500

    :wrench: Update conf

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,45 +1,4 @@
 const colors = require('tailwindcss/colors');
-const cssWhitelistClassArray = [
-  'tippy',
-  'leaflet',
-  'leaflet-container',
-  'leaflet-controls-container',
-  'pill',
-  'orange',
-  'red',
-  'yellow',
-  'green',
-  'purple',
-  'bg-blue-400',
-  'bg-blue-500',
-  'bg-blue-600',
-  'bg-purple-400',
-  'bg-purple-500',
-  'bg-purple-600',
-  'opacity-75',
-  'bg-gray-50',
-  'bg-gray-100',
-  'bg-gray-200',
-  'bg-gray-300',
-  'bg-gray-400',
-  'bg-gray-500',
-  'bg-gray-600',
-  'bg-gray-700',
-  'bg-gray-800',
-  'bg-gray-900',
-  'dark:bg-gray-50',
-  'dark:bg-gray-100',
-  'dark:bg-gray-200',
-  'dark:bg-gray-300',
-  'dark:bg-gray-400',
-  'dark:bg-gray-500',
-  'dark:bg-gray-600',
-  'dark:bg-gray-700',
-  'dark:bg-gray-800',
-  'dark:bg-gray-900',
-  'bg-white',
-  'ct-toast-info',
-];
 
 module.exports = {
   darkMode: 'class',
@@ -49,15 +8,6 @@ module.exports = {
       './src/components/**/*.js',
       './src/modules/helpers.js',
     ],
-    preserveHtmlElements: false,
-    options: {
-      safeList: cssWhitelistClassArray,
-      defaultExtractor: (content) => {
-        const broadMatches = content.match(/[^<>"'`\s]*[^<>"'`\s:]/g) || [];
-        const innerMatches = content.match(/[^<>"'`\s.()]*[^<>"'`\s.():]/g) || [];
-        return broadMatches.concat(innerMatches);
-      },
-    },
   },
   theme: {
     colors: {
