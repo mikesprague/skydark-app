2eea393c21626916231d356c02c144d6b822c057
Michael Sprague <mikesprague@gmail.com>
2020-12-21 22:27:19 -0500

    :sparkles: Add default settings

---
+++ src/modules/default-settings.js
@@ -0,0 +1,12 @@
+export const defaultAppSettings = {
+  // system|light|dark|sunrise-sunset
+  theme: 'system',
+  // 10|15|20|25|30|45|60
+  cacheTtl: '10',
+  // same-as-theme|dark|light|color|street|street-gray|black-white|black-white-gray|watercolor
+  mapStyle: 'same-as-theme',
+  // auto|us|si|uk2|ca
+  weatherUnits: 'auto',
+};
+
+export default defaultAppSettings;
