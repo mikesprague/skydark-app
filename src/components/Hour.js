c9fd90ce73e3757b51da294ea88773cbfd64c557
Michael Sprague <mikesprague@gmail.com>
2021-05-30 09:09:42 -0400

    :recycle: Improve logic

--- src/components/Hour.js
+++ src/components/Hour.js
@@ -40,7 +40,7 @@ export const Hour = ({ data, dayData, summary, isFirst, isLast, conditionToShow,
         onClick={clickHandler}
         style={
           ['temperature', 'apparentTemperature', 'dewPoint'].includes(conditionToShow)
-            ? { marginRight: `${Math.round(100 - (Math.floor(data[conditionToShow]) * valScale))}%` }
+            ? { marginRight: `${Math.round(100 - (Math.round(data[conditionToShow]) * valScale))}%` }
             : {}
         }
       >
