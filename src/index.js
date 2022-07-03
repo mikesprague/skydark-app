65f6695ca3ea76514b88c36e418d7782eeb1a9a4
Michael Sprague <mikesprague@gmail.com>
2022-07-03 10:57:10 -0400

    :recycle: Use react 18 syntax

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 import Bugsnag from '@bugsnag/js';
 import BugsnagPluginReact from '@bugsnag/plugin-react';
 import LogRocket from 'logrocket';
-import setupLogRocketReact from 'logrocket-react';
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
+import setupLogRocketReact from 'logrocket-react';
+
 import './index.scss';
 import { App } from './components/App';
 import { ErrorView } from './components/ErrorView';
@@ -19,12 +20,15 @@ setupLogRocketReact(LogRocket);
 
 const ErrorBoundary = Bugsnag.getPlugin('react').createErrorBoundary(React);
 
-ReactDOM.render(
+const container = document.getElementById('root');
+const root = createRoot(container);
+
+root.render(
   <ErrorBoundary FallbackComponent={ErrorView}>
     <React.StrictMode>
       <App OPENWEATHERMAP_API_KEY={process.env.OPENWEATHERMAP_API_KEY} />
     </React.StrictMode>
-  </ErrorBoundary>, document.getElementById('root'),
+  </ErrorBoundary>,
 );
 
 initSkyDark();
