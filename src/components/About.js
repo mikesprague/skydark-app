11c12c0a9623d6f6ede8436fc84f7a4c0541a22f
Michael Sprague <mikesprague@gmail.com>
2022-08-06 16:43:50 +0000

    :lipstick: Tweak header

--- src/components/About.js
+++ src/components/About.js
@@ -6,10 +6,14 @@ import './About.scss';
 
 export const About = () => (
   <div className="contents">
-    <div className="header">
+    <div className="header not-fixed">
       <div className="section-name">
         <h1>
-          <FontAwesomeIcon icon={['fas', 'circle-info']} className="footer-icon" fixedWidth />
+          <FontAwesomeIcon
+            icon={['fas', 'circle-info']}
+            className="footer-icon"
+            fixedWidth
+          />
           {' About'}
         </h1>
       </div>
@@ -18,7 +22,12 @@ export const About = () => (
       <h2>Background</h2>
       <p>
         {'Sky Dark is an open source '}
-        <Tippy content="Progressive Web Application" arrow={true} placement="auto" trigger="click">
+        <Tippy
+          content="Progressive Web Application"
+          arrow={true}
+          placement="auto"
+          trigger="click"
+        >
           <abbr title="Progressive Web Application">PWA</abbr>
         </Tippy>
         {' built as an homage to the now defunct Dark Sky for Android app.'}
@@ -26,14 +35,26 @@ export const About = () => (
       <p>
         More details here:
         <br />
-        <a href="https://github.com/mikesprague/skydark-app#readme" rel="noopener noreferrer" target="_blank">https://github.com/mikesprague/skydark-app#readme</a>
+        <a
+          href="https://github.com/mikesprague/skydark-app#readme"
+          rel="noopener noreferrer"
+          target="_blank"
+        >
+          https://github.com/mikesprague/skydark-app#readme
+        </a>
       </p>
       <h2>Issues/Bugs</h2>
       <p>Errors are tracked/reported automatically with Bugsnag.</p>
       <p>
         Bug reports can be manually filed here:
         <br />
-        <a href="https://github.com/mikesprague/skydark-app/issues" rel="noopener noreferrer" target="_blank">https://github.com/mikesprague/skydark-app/issues</a>
+        <a
+          href="https://github.com/mikesprague/skydark-app/issues"
+          rel="noopener noreferrer"
+          target="_blank"
+        >
+          https://github.com/mikesprague/skydark-app/issues
+        </a>
       </p>
     </div>
   </div>
