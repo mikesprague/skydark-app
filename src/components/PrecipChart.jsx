01fe3c1dfb14984dfd0906249f29af55b300be61
Michael Sprague <mikesprague@gmail.com>
2023-01-11 11:04:41 -0500

    refactor(PrecipChart): limit to 60 minutes (api returns 85)

--- src/components/PrecipChart.jsx
+++ src/components/PrecipChart.jsx
@@ -16,9 +16,11 @@ export const PrecipChart = () => {
 
     const dataArray = [['Minute', 'Precipitation']];
 
-    data.weather.forecastNextHour.minutes.forEach((minute, index) => {
-      dataArray.push([index, minute.precipitationIntensity]);
-    });
+    data.weather.forecastNextHour.minutes
+      .slice(0, 59)
+      .forEach((minute, index) => {
+        dataArray.push([index, minute.precipitationIntensity]);
+      });
     setChartData(dataArray);
   }, [data]);
 
