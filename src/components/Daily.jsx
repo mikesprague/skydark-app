00a9a167a005465ac1cd4882901b20aa2e2aa96d
Michael Sprague <mikesprague@gmail.com>
2025-01-05 10:00:02 -0500

    refactor(Daily): logic improvements

--- src/components/Daily.jsx
+++ src/components/Daily.jsx
@@ -11,49 +11,40 @@ import { useWeatherDataContext } from '../contexts/WeatherDataContext.jsx';
 
 export const Daily = () => {
   const [dailyData, setDailyData] = useState(null);
+  const [minLow, setMinLow] = useState(0);
+  const [maxHigh, setMaxHigh] = useState(0);
 
   const { weatherData: weather } = useWeatherDataContext();
 
   useEffect(() => {
-    if (!weather) {
-      return;
+    if (weather) {
+      setDailyData(weather);
     }
-
-    setDailyData(weather);
   }, [weather]);
 
-  const [minLow, setMinLow] = useState(0);
-  const [maxHigh, setMaxHigh] = useState(0);
-
   useEffect(() => {
-    if (!dailyData) {
-      return;
+    if (dailyData) {
+      const slicedData = dailyData.forecastDaily.days.slice(0, 8);
+      const allLows = slicedData.map((val) => val.temperatureMin);
+      const allHighs = slicedData.map((val) => val.temperatureMax);
+
+      setMinLow(Math.round(metricToImperial.cToF(Math.min(...allLows))));
+      setMaxHigh(Math.round(metricToImperial.cToF(Math.max(...allHighs))));
     }
-
-    const slicedData = dailyData.forecastDaily.days.slice(0, 8);
-    const allLows = slicedData.map((val) => val.temperatureMin);
-    const allHighs = slicedData.map((val) => val.temperatureMax);
-
-    setMinLow(Math.round(metricToImperial.cToF(Math.min(...allLows))));
-    setMaxHigh(Math.round(metricToImperial.cToF(Math.max(...allHighs))));
   }, [dailyData]);
 
   return dailyData?.forecastDaily ? (
     <div className="daily-container">
       <div className="daily">
-        {dailyData.forecastDaily.days.map((dayData, dayIndex) => {
-          return dayIndex <= 7 ? (
-            <Day
-              key={nanoid(7)}
-              data={dayData}
-              dayIndex={dayIndex}
-              minLow={minLow}
-              maxHigh={maxHigh}
-            />
-          ) : (
-            ''
-          );
-        })}
+        {dailyData.forecastDaily.days.slice(0, 8).map((dayData, dayIndex) => (
+          <Day
+            key={nanoid(7)}
+            data={dayData}
+            dayIndex={dayIndex}
+            minLow={minLow}
+            maxHigh={maxHigh}
+          />
+        ))}
       </div>
     </div>
   ) : (
